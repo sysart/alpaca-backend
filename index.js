ff11c3f00469754d5def84950bb8ad62930a0c70
Pekka Astala <pekka.astala@sysart.fi>
2019-09-27 16:15:38 +0300

    we're gonna be rich

--- index.js
+++ index.js
@@ -1,12 +1,74 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const cors = require('cors');
+const Alpaca = require('@alpacahq/alpaca-trade-api');
+require('dotenv').config();
 
 const app = express();
 
-app.use(cors());
+const alpaca = new Alpaca({
+  keyId: process.env.ALPACA_KEY_ID,
+  secretKey: process.env.ALPACA_KEY_SECRET,
+  paper: true,
+});
 
-app.get('/get-stocks', (req, res) => {
-  res.json([]);
+app.use(cors());
+app.use(bodyParser.json());
+
+app.post('/orders', async (req, res) => {
+  const { symbol, qty } = req.body;
+  await alpaca.createOrder({
+    symbol,
+    qty,
+    side: 'buy',
+    type: 'market',
+    time_in_force: 'day',
+  });
+  res.send('ok');
+});
+
+app.get('/position/:symbol', (req, res) => {
+  alpaca.getPosition(req.params.symbol).then((pos) => {
+    res.json(pos);
+  });
+});
+
+app.post('/do-trade-if-it-is-reasonable/:symbol', async (req, res) => {
+  const { symbol } = req.params;
+  let pos;
+  try {
+    pos = await alpaca.getPosition(symbol);
+  } catch (e) {
+    res.status(400).json({
+      status: 'error',
+      message: 'could not get position',
+    });
+  }
+
+  const percentage = Number(pos.unrealized_intraday_pl);
+
+  if (percentage < -0.1) {
+    const orderQty = Number(pos.qty) * 2;
+    await alpaca.createOrder({
+      symbol,
+      qty: orderQty,
+      side: 'buy',
+      type: 'market',
+      time_in_force: 'day',
+    });
+    res.json({
+      status: 'ordered',
+      qty: orderQty,
+    });
+  } else if (percentage > 0.2) {
+    await alpaca.closePosition(symbol);
+    res.json({
+      status: 'closed',
+    });
+  }
+  res.json({
+    status: 'nothing',
+  });
 });
 
 const port = process.env.PORT || 3001;
