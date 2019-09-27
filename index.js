ce6b4997481750d692c71fab0069bd218f2905d2
Pekka Astala <pekka.astala@sysart.fi>
2019-09-27 10:58:32 +0300

    Added cors

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
 
+app.use(cors());
+
 app.get('/', (req, res) => {
-  res.json([]);
+  res.json([
+    {
+      tradeId: 1,
+      buyer: 'Pekka',
+      wasAGoodTrade: true,
+    },
+  ]);
 });
 
 const port = process.env.PORT || 3001;
